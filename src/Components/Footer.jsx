989be79852d382475e808a8ed4407772626e3c5f
gearkid1127 <gearkid1127@gmail.com>
2025-10-17 01:06:35 -0500

    Fix Footer.css import case for Vercel build

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import '../styles/footer.css'
+import '../styles/Footer.css'
 import { useUI } from "../Context/UIContext.jsx";
 
 const Footer = () => {
